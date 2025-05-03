879827ae59abd0d8b3b4cbe78fe5908e6c38d582
Pedro Soares <157700905+pedrowayt@users.noreply.github.com>
2025-05-03 19:16:21 -0300

    update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "04ec1508f60b42d3"; }
+/******/ 		__webpack_require__.h = function() { return "935dc9bd8a6815d9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
