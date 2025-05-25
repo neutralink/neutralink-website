1a0c70e83177831dc2e2924dd56c2850b288e90c
Pedro Soares <157700905+pedrowayt@users.noreply.github.com>
2025-05-24 23:50:29 -0300

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import { ReactNode } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 export const metadata = {
   title: 'NeutraLink',
@@ -12,9 +13,11 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="pt-BR">
       <body className="flex flex-col min-h-screen text-foreground">
-        <Header />
-        <main className="flex-grow">{children}</main>
-        <Footer />
+        <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}>
+          <Header />
+          <main className="flex-grow">{children}</main>
+          <Footer />
+        </GoogleOAuthProvider>
       </body>
     </html>
   );
