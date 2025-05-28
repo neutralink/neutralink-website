7f46b610b89e40d253a4469904ec572520dc9c62
Pedro Soares <157700905+pedrowayt@users.noreply.github.com>
2025-05-27 22:08:03 -0300

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,8 @@
-
-
 import './globals.css';
 import { ReactNode } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { GoogleOAuthProvider } from '@react-oauth/google';
-import { usePathname } from 'next/navigation';
 
 export const metadata = {
   title: 'NeutraLink',
@@ -13,26 +10,13 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-  const pathname = usePathname();
-  const isDashboardRoute = pathname?.startsWith('/dashboard');
-
   return (
     <html lang="pt-BR">
       <body className="flex flex-col min-h-screen text-foreground">
         <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}>
-          {!isDashboardRoute && <Header />}
-          {isDashboardRoute && (
-            <div className="hidden md:block">
-              <Header />
-            </div>
-          )}
-          <main className="flex-grow">{children}</main>
-          {!isDashboardRoute && <Footer />}
-          {isDashboardRoute && (
-            <div className="hidden md:block">
-              <Footer />
-            </div>
-          )}
+          <Header />
+          <main className="flex-1">{children}</main>
+          <Footer />
         </GoogleOAuthProvider>
       </body>
     </html>
