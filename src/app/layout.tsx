61e576b8b826a4a455439b532ac005650ef6a6ec
Pedro Soares <157700905+pedrowayt@users.noreply.github.com>
2025-05-27 22:19:35 -0300

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,11 @@
+"use client";
+
 import './globals.css';
 import { ReactNode } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { usePathname } from 'next/navigation';
-
-export const metadata = {
-  title: 'NeutraLink',
-  description: 'Plataforma de créditos de carbono tokenizados e acessíveis',
-};
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   const pathname = usePathname();
